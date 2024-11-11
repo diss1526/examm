a7d8174891f7c6a0f00661f3c20f7f6a31f22a46
diss1526 <dm1569525@gmail.com>
2024-11-11 18:43:06 +0500

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,52 @@
+var number =  prompt("1-ден 12-ге дейін сан енгізіңіз:")
+
+switch(number){
+    case "1":
+        alert("Ай 1, Маусым: Winter");
+        break;
+    
+    case "2":
+        alert("Ай 2, Маусым: Winter");
+        break;
+
+    case "3":
+        alert("Ай 3, Маусым: Spring");
+        break;
+
+    case "4":
+        alert("Ай 4, Маусым: Spring");
+        break;
+            
+    case "5":
+        alert("Ай 5, Маусым: Spring");
+        break;
+
+    case "6":
+        alert("Ай 6, Маусым: Summer");
+        break;
+    
+    case "7":
+        alert("Ай 7, Маусым: Summer");
+        break;
+
+    case "8":
+        alert("Ай 8, Маусым: Summer");
+        break;
+    
+    case "9":
+        alert("Ай 9, Маусым: Autuum ");
+        break;
+
+    case "10":
+        alert("Ай 10, Маусым: Autuum");
+        break;
+
+    case "11":
+        alert("Ай 11, Маусым: Autuum");
+        break;
+    
+    case "12":
+        alert("Ай 12, Маусым: Winter");
+        break;
+}
+
---
+++ task3.js
@@ -0,0 +1,8 @@
+var a = Number(prompt("a санын енгізіңіз:"))
+var b = Number(prompt("b санын енгізіңіз:"))
+
+if(a < b){
+    alert("a саны b санынан кіші")
+}else{
+    alert("a саны b санынан үлкен")
+}
